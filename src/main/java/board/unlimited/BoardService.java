2c8182413cb4c7bb02a58e85dd4df7abdaac9f9d
yj0913k <107930703+yj0913k@users.noreply.github.com>
2023-03-31 12:58:42 +0900

    계층형 시작 이거 안되면 롤백해야함 ***롤백지점***

--- src/main/java/board/unlimited/BoardService.java
+++ src/main/java/board/unlimited/BoardService.java
@@ -2,57 +2,17 @@ package board.unlimited;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
 
-public class BoardService{
-    private BoardRepository boardRepository;
-
-    //entity 값을 DTO로 변환하여 리스트에 하나씩 넣어줌
-    private BoardDTO dtos(Board boardEntity) {
-        return BoardDTO.builder()
-                .id(boardEntity.getId())
-                .title(boardEntity.getTitle())
-                .content(boardEntity.getContent())
-                .parentNum(boardEntity.getId())
-                .childNum(boardEntity.getChildNum())
-                .depth(boardEntity.getDepth())
-                .build();
-    }
-
-    @Transactional
-    public Long getBoardCount() {
-        return boardRepository.count();
-    }
-
-
-    //게시글 상세페이지
-    public BoardDTO detail(Long id) {
-        Optional<Board> board = boardRepository.findById(id);
-        Board boardId = board.get();
-        BoardDTO boardDTO = BoardDTO.builder()
-                .id(boardId.getId())
-                .title(boardId.getTitle())
-                .content(boardId.getContent())
-                .parentNum(boardId.getParentNum())
-                .childNum(boardId.getChildNum())
-                .depth(boardId.getDepth())
-                .build();
-
-        return boardDTO;
-
-    }
-
-
-
-    public void deleteById(Long id) {
-        boardRepository.deleteById(id);
-    }
-
-
+public interface BoardService{
+    public Long getBoardCount();
+    public BoardDTO detail(Long id);
+    public void deleteById(Long id);
 
 
 }
