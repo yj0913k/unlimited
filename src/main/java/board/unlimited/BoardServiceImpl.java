300db71a3edea11cb6b61bbcbebda24ff3092201
yj0913k <107930703+yj0913k@users.noreply.github.com>
2023-03-31 12:58:52 +0900

    계층형 시작 이거 안되면 롤백해야함 ***롤백지점***

---
+++ src/main/java/board/unlimited/BoardServiceImpl.java
@@ -0,0 +1,40 @@
+package board.unlimited;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+    @Override
+    public Long getBoardCount() {
+        return boardRepository.count();    }
+
+    @Override
+    public BoardDTO detail(Long id) {
+        Optional<Board> board = boardRepository.findById(id);
+        Board boardId = board.get();
+        BoardDTO boardDTO = BoardDTO.builder()
+                .id(boardId.getId())
+                .title(boardId.getTitle())
+                .content(boardId.getContent())
+                .parentNum(boardId.getParentNum())
+                .childNum(boardId.getChildNum())
+                .depth(boardId.getDepth())
+                .build();
+
+        return boardDTO;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
